772410814f5c4214cdc01fa3b9ca72e40df75e1b
Dave Null <DaveNull@yeet.hell>
2022-05-27 15:42:13 +0300

    helper bash scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 cd packages/x86/include
-for file in `find ../../../ -name '*.asm'`
+for file in `find ../../../packages -name '*.asm'`
 do
     fasmg $file $file.out && chmod +x $file.out &
 done
